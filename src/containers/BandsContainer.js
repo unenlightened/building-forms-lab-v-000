c36f4d3885c499ce1e18f238aced76363130d8e7
Chely Ho <adept_inept@yahoo.com>
2018-10-04 01:18:29 +0000

    automatically backed up by learn

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import BandInput from '../components/BandInput'
 
 class BandsContainer extends Component {
   render() {
     return(
       <div>
-        BandsContainer
+        <BandInput />
       </div>
     )
   }
